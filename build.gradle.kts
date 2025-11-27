8f32103350f23c6eda5a36aeac09e01d28851f71
jamestoney311 <jamestoney311@gmail.com>
2025-11-27 15:17:18 +0800

    Changed version to 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 group = "io.github.jamestoney311"
 val artifactId = "org.eclipse.birt.report.engine.emitter.csv"
-version = "1.0.11"
+version = "1.0.2"
 
 repositories {
     mavenCentral()
